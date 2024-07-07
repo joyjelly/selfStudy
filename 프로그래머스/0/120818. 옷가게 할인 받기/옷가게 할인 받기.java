db92711b9ef35347b1b194794ab6ca2936c8d580
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-07 22:42:09 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(int price) {
-        double finalPrice = price;
-
-        if (price >= 500000) {
-            finalPrice = price * 0.8; 
-        } else if (price >= 300000) {
-            finalPrice = price * 0.9; 
-        } else if (price >= 100000) {
-            finalPrice = price * 0.95; 
-        }
-
-        return (int) finalPrice;
+        int answer = 0;
+        return answer;
     }
 }
