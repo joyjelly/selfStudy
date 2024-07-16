0466dcc11ada770e6297e4f1425131dbf297d80b
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-16 19:56:00 +0900

    [level 0] Title: 모스부호 (1), Time: 0.33 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,39 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
-
 class Solution {
-    // 모스 부호와 영어 소문자의 대응 관계를 저장할 맵을 초기화합니다.
-    private static final Map<String, Character> morseToAlphabet = new HashMap<>();
-
-    static {
-        // 알파벳과 모스 부호 배열
-        String[] morseCodes = {
-            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
-            "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
-            "..-", "...-", ".--", "-..-", "-.--", "--.."
-        };
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        
-        // 매핑 초기화
-        for (int i = 0; i < morseCodes.length; i++) {
-            morseToAlphabet.put(morseCodes[i], alphabet.charAt(i));
-        }
-    }
-
     public String solution(String letter) {
-        // 공백을 기준으로 모스 부호를 분할합니다.
-        String[] trans = letter.split(" ");
-        StringBuilder answer = new StringBuilder();
-
-        for (String morseLetter : trans) {
-            // 모스 부호를 알파벳 문자로 변환합니다.
-            Character decodedChar = morseToAlphabet.get(morseLetter);
-            if (decodedChar != null) {
-                answer.append(decodedChar);
+        Map<String,Character> map = new HashMap<>();
+        String[] codes ={ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String alpabet ="abcdefghijklmnopqrstuvwxyz";
+        for(int i =0;i<codes.length;i++){
+            map.put(codes[i], alpabet.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        String[] trans =letter.split(" ");
+        for(String temp :trans){
+            Character transChar = map.get(temp);
+            if(transChar !=null){
+                sb.append(transChar);
             }
         }
         
-        return answer.toString();
-    }}
-
- 
+        
+       
+        return sb.toString();
+    }
+}
