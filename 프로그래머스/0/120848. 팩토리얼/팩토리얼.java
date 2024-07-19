c6b4b35e2cf8ae8494025058271336cc1111be34
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-19 21:16:02 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -2,13 +2,15 @@ class Solution {
     public int solution(int n) {
         int i = 1;
         int factorial = 1;
-        
-        while (true) {
-            if (factorial > n) {
-                return i - 1;
-            }
-            i++;
-            factorial *= i;
-        }
+       while(true){
+           if(factorial>n){
+               return i-1;
+               
+           }
+               i++;
+               factorial *= i;
+           }
+       } 
+       
     }
-}
+
