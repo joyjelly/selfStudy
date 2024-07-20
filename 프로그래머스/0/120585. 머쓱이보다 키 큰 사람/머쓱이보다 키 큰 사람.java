432bafdf3ad38ea25068719e9523ed2b29645590
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-20 15:58:02 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
+
 class Solution {
     public int solution(int[] array, int height) {
-        Arrays.sort(array);
-         int answer = 0;
-        for(int temp:array){
+        int answer = 0;
+        for(int temp : array){
             if(temp>height){
                 answer++;
             }
         }
-       
         return answer;
     }
 }
