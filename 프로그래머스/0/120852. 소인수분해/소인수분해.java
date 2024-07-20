617b433effb02304390813592e43d283070d669c
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-20 21:31:02 +0900

    [level 0] Title: 소인수분해, Time: 0.92 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+      Set<Integer> set = new TreeSet <Integer>();
+        for(int i =2; i<=n; i++){
+            while(n%i==0){
+                set.add(i);
+                n /=i;
+            }
+        }
+        int[] answer = new int[set.size()];
+        int index =0;
+        for(int temp : set){
+            answer[index++] = temp;
+        }
+        return answer;
+    }
+}
