c326b31091f84b5181aa13c3100479d6352fcb45
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-20 19:44:56 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 2; i <= n; i += 2) {  // 2부터 n까지 2씩 증가하는 반복문
-            answer += i;  // 짝수를 더함
+        for(int i =2;i<=n;i+=2){
+            answer +=i;
         }
         return answer;
     }
