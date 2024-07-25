80c0838a10a1019afbba75a607ae6f4d6e9a68dc
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-07-25 15:43:15 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.21 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(String my_string) {
-       
+
         String[] parts = my_string.split(" ");
-        
-  
-        int result = Integer.parseInt(parts[0]);
-        
      
+        int result = Integer.parseInt(parts[0]);
+  
         for (int i = 1; i < parts.length; i += 2) {
             String operator = parts[i];
             int number = Integer.parseInt(parts[i + 1]);
