0ceab83b05941cc912ca58ba2ed7cbaa75967c33
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-10-02 11:59:46 +0900

    [level 1] Title: 폰켓몬, Time: 15.03 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int[] nums) {
+       
+        HashSet set = Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));
+        int n = nums.length;
+        int k = n/2;
+        
+        
+        
+        
+        return Math.min(k,set.size());
+    }
+}
