f86d5dd484fceb24fc2928567b8c65a280d123f0
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-10-09 00:04:50 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,22 +1,33 @@
+import java.util.*;
 class Solution {
+   private static int width;
+   private static int height;
+   private boolean isvaild(int x, int y, int nx,int ny){
+       return Math.abs(x+nx)<=width&&Math.abs(y+ny)<=height;
+       } 
     public int[] solution(String[] keyinput, int[] board) {
-        int[] answer = {0, 0};
+      int x =0;
+    int y = 0;
         
-        int height = board[1] / 2;
-        int width = board[0] / 2;
-        
-        for(String temp : keyinput) {
-            if(temp.equals("up") && answer[1] < height) {
-                answer[1]++;
-            } else if(temp.equals("down") && answer[1] > -height) {
-                answer[1]--;
-            } else if(temp.equals("right") && answer[0] < width) {
-                answer[0]++;
-            } else if(temp.equals("left") && answer[0] > -width) {
-                answer[0]--;
+    Map<String, int[]> moves = new HashMap<>();
+        moves.put("up",new int[]{0,1});
+        moves.put("down",new int[]{0,-1});
+        moves.put("right",new int[]{1,0});
+        moves.put("left",new int[]{-1,0});
+        width =board[0]/2;
+        height= board[1]/2;
+        for(String temp:keyinput){
+            int nx = moves.get(temp)[0];
+            int ny = moves.get(temp)[1];
+            
+            if(isvaild(x,y,nx,ny)){
+                x+=nx;
+                y+=ny;
             }
+            
         }
         
-        return answer;
+        
+        return new int[]{x,y};
     }
 }
