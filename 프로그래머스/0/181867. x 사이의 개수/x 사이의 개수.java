3af19bab929fed46fee153f7b3329e94306a3449
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-10-10 16:28:43 +0900

    [level 0] Title: x 사이의 개수, Time: 53.41 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String myString) {
+        String[] temp = myString.split("x",-1);
+        int[] answer = new int[temp.length];
+        
+        for(int i =0;i<temp.length;i++){
+            System.out.println(temp[i]);
+            answer[i] = temp[i].length();
+        }
+        
+        return answer;
+    }
+}
