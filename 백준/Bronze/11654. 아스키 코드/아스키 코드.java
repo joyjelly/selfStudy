4a719ed54e1f80eb1372ebe094b7ff924cfeced5
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-10-23 17:02:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+       
+            String a = in.nextLine();
+            char b = a.charAt(0);
+             System.out.println((int)b);
+        
+      
+       
+    }
+}
