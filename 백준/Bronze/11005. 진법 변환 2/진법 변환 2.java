222ca51e8ccbf318450560ad3074450794e7fec6
joyjelly <159707885+joyjelly@users.noreply.github.com>
2024-10-26 22:56:38 +0900

    [Bronze I] Title: 진법 변환 2, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();  
+        int B = in.nextInt();  
+        System.out.println(Integer.toString(N, B).toUpperCase()); 
+    }
+}
