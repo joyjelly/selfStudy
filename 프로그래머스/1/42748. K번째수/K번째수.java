e7499b9c8cec33bce14d01ea397f2807bb003fb1
joyjelly <159707885+joyjelly@users.noreply.github.com>
2025-11-20 21:55:57 +0900

    [level 1] Title: K번째수, Time: 0.38 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for(int i =0;i<commands.length;i++){
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int result = commands[i][2];
+        for (int i = 0 ; i<commands.length;i++){
+            int st =commands[i][0];
+            int ed =commands[i][1];
+            int value =commands[i][2];
             
-            int[] temp = Arrays.copyOfRange(array,start-1,end);
+            int[] temp = Arrays.copyOfRange(array,st-1,ed);
             Arrays.sort(temp);
-            
-            answer[i] = temp[result-1];
+            answer[i] = temp[value-1];
             
         }
-        
         
         return answer;
     }
